5fc65421cccd7a0139154b89f32d63cba450197c
Finch0714 <156517199+Finch0714@users.noreply.github.com>
2026-04-05 21:26:14 +0800

    Update update_steam.py

--- update_steam.py
+++ update_steam.py
@@ -16,7 +16,7 @@ MANUAL_GAMES = [
     },
     {
         "appid": "manual_02",
-        "name": "Minecraft",
+        "name": "Minecraft Java Edition",
         "playtime": 832,
         "icon": "https://wallpaperm.cmcm.com/182ceb54d84374469e5ea079a4f0befc.jpg" # 这里可以放任何图片的直连链接
     }
