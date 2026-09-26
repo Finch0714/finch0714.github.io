0a1313dc4f3d155a74e9496e0b3eab6a6353d52e
finch0714 <aaabbb0714@163.com>
2026-09-26 19:59:56 +0800

    🔒 steam + letter: 隐藏信件密码提示，更新游戏时长
    letter.html
      正文里「至于为什么密码是 0905」改成「至于密码的含义，是这个想法诞生的日子」，
      代码注释里的「0905 的双重 MD5」也改成「双重 MD5」，
      不再在页面和源码里留下明文密码提示。
    
    assets/steam.js
      Cyberpunk 2077 游玩时长 45 -> 61 小时。

--- assets/steam.js
+++ assets/steam.js
@@ -18,7 +18,7 @@ window.STEAM_DATA = {
     {
       appid: "manual_01",
       name: "Cyberpunk 2077",
-      playtime: 45,
+      playtime: 61,
       icon: "https://ts1.tc.mm.bing.net/th/id/R-C.4766d3a3044fc96967d19538153043c2?rik=Rq1FI%2fX5qARXSA&pid=ImgRaw&r=0",
     },
     {
